51a78b0d1f6e7b9c128c4cce437f833115459898
gvijay00 <gurugubellivijayakumar.com>
2025-04-11 00:23:12 +0530

    ReverseForLoop

---
+++ java_codes_36r/src/loops/ReverseForLoop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class ReverseForLoop {
+	public static void main(String[] args) {
+		// Print numbers from 10 to 1 using reverse for loop
+		for (int i = 10; i >= 1; i--) {
+			System.out.println(i);
+		}
+	}
+}
