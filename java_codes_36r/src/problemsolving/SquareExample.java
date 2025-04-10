b20bbd246fc16d96663ec933a5ba7fb0932aae1b
gvijay00 <gurugubellivijayakumar.com>
2025-04-11 00:36:21 +0530

    SquareExample

---
+++ java_codes_36r/src/problemsolving/SquareExample.java
@@ -0,0 +1,10 @@
+package problemsolving;
+
+public class SquareExample {
+	public static void main(String[] args) {
+		int number = 5;
+		int square = number * number;
+
+		System.out.println("Square of " + number + " is: " + square);
+	}
+}
