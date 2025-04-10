65a38c7907d3382d49c28f1a3b8d7f9942ad0315
gvijay00 <gurugubellivijayakumar.com>
2025-04-11 00:32:35 +0530

    SumOfEvenNumbers

--- java_codes_36r/src/problemsolving/SumOfEvenNumbers.java
+++ java_codes_36r/src/problemsolving/SumOfEvenNumbers.java
@@ -9,5 +9,15 @@ public class SumOfEvenNumbers {
 		}
 
 		System.out.println("Sum of even numbers from 1 to 50 is: " + sum);
+		
+//		int sum = 0;
+//		
+//		for (int i = 1; i <= 50; i++) {
+//		    if (i % 2 == 0) {
+//		        sum += i;
+//		    }
+//		}
+//		System.out.println("Sum of even numbers from 1 to 50 is: " + sum);
+
 	}
 }
